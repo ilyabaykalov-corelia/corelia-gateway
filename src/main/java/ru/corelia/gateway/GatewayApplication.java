ee7aba1b5056d8eef4b6d4dd07a90e2fe238e603
ilyabaykalov <ibaykalov@blogic.ru>
2026-09-08 17:35:16 +0300

    Создан каркас сервиса corelia-gateway

---
+++ src/main/java/ru/corelia/gateway/GatewayApplication.java
@@ -0,0 +1,27 @@
+package ru.corelia.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ru.corelia.config.LocalEnvironment;
+
+/** Запускает приложение corelia-gateway. */
+@SpringBootApplication(
+        scanBasePackages = {
+            "ru.corelia.config",
+            "ru.corelia.profile",
+            "ru.corelia.support",
+            "ru.corelia.auth",
+            "ru.corelia.http",
+            "ru.corelia.cache",
+            "ru.corelia.integration",
+            "ru.corelia.transport",
+            "ru.corelia.gateway"
+        })
+public class GatewayApplication {
+    public static void main(String[] args) {
+        var app = new SpringApplication(GatewayApplication.class);
+        app.setDefaultProperties(LocalEnvironment.load());
+        app.run(args);
+    }
+}
